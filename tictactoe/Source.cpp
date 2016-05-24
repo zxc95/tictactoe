666707872d4bb2ef32cb7635a668b553c82e535e
echo <ilyakovey@gmail.com>
2016-05-24 19:16:22 +0500

    v4.0

--- tictactoe/Source.cpp
+++ tictactoe/Source.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 //Глобальные константы
 const char EMPTY = ' ';
+const char X = 'X';
+const char O = 'O';
+const char NO_ONE = 'N';
 
 //Прототипы функций
 void instructions();
@@ -12,17 +15,36 @@ char askYesNo(string question);
 char humanPiece();
 char opponent(char piece);
 void displayBoard(const vector<char>& board);
+char winner(const vector<char>& board);
+int humanMove(const vector<char>& board, char human);
+int computerMove(vector<char> board, char computer);
 
 int main() {
+	int move;
 	const int NUM_SQUARES = 9;
 	vector<char> board(NUM_SQUARES, EMPTY);
 
 	instructions();
 	char human = humanPiece();
 	char computer = opponent(human);
-	char turn = 'X';
+	char turn = X;
 	displayBoard(board);
-	
+
+	while (winner(board) == NO_ONE)
+	{
+		if (turn == human)
+		{
+			move = humanMove(board, human);
+			board[move] = human;
+		}
+		else
+		{
+			move = computerMove(board, computer);
+			board[move] = computer;
+		}
+		displayBoard(board);
+		turn = opponent(turn);
+	}
 
 	system("pause");
 	return 0;
@@ -58,23 +80,23 @@ char humanPiece()
 	char go_first = askYesNo("Вы будете ходить первым?");
 	if (go_first == 'y')
 	{
-		return 'X';
+		return X;
 	}
 	else
 	{
-		return 'O';
+		return O;
 	}
 }
 
 char opponent(char piece)
 {
-	if (piece == 'X')
+	if (piece == X)
 	{
-		return 'O';
+		return O;
 	}
 	else
 	{
-		return 'X';
+		return X;
 	}
 }
 
